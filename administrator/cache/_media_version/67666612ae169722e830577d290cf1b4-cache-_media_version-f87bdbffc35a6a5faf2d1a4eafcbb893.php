8c292523c385ef4d79a4db61a20fe55168e592b1
adamant-213 <adambern19@gmail.com>
2025-10-13 09:57:42 -0400

    Hello Module S4

--- administrator/cache/_media_version/67666612ae169722e830577d290cf1b4-cache-_media_version-f87bdbffc35a6a5faf2d1a4eafcbb893.php
+++ administrator/cache/_media_version/67666612ae169722e830577d290cf1b4-cache-_media_version-f87bdbffc35a6a5faf2d1a4eafcbb893.php
@@ -1,1 +1,1 @@
-<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:6:"b3e626";s:6:"output";s:0:"";}
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:6:"259c53";s:6:"output";s:0:"";}
--- modules/mod_hello/src/Dispatcher/dispatcher.php
+++ modules/mod_hello/src/Dispatcher/dispatcher.php
@@ -6,15 +6,20 @@ namespace My\Module\Hello\Site\Dispatcher;
 
 use Joomla\CMS\Dispatcher\DispatcherInterface;
 use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 use My\Module\Hello\Site\Helper\HelloHelper;
 
 class Dispatcher implements DispatcherInterface
 {
     public function dispatch()
     {
+        $language = Factory::getApplication()->getLanguage();
+        $language->load('mod_hello', JPATH_BASE . '/modules/mod_hello');
+        
         $username = HelloHelper::getLoggedonUsername('Guest');
 
-        $hello = "Hello {$username}";
+        $hello = Text::_('MOD_HELLO_GREETING') . $username;
 
         require ModuleHelper::getLayoutPath('mod_hello');
     }
